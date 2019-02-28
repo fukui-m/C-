5d193e3c74348cbe310cfe6db42ff2c1e1526559
fukui-m <48060397+fukui-m@users.noreply.github.com>
2019-02-28 17:57:10 +0900

    Add files via upload

---
+++ 3day/3day test5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("整数値を入力してください");
+            int firstInputValue = int.Parse(Console.ReadLine());
+            if (30 < firstInputValue && firstInputValue<= 70)
+            {
+                Console.WriteLine("30より大きく70以下です");
+            }
+        }
+    }
+}
