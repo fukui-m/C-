38d0b7102f40be633db211c0ef8416f447a9f5ac
fukui-m <48060397+fukui-m@users.noreply.github.com>
2019-02-28 14:06:36 +0900

    Add files via upload

---
+++ 2day/2day test4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1;
+            Console.Write("文字列を入力:");
+            str1=Console.ReadLine();
+            Console.WriteLine("入力された文字列:{0}", str1);
+        }
+    }
+}
---
+++ 2day/2day test5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1, str2;
+            Console.Write("文字列1を入力");
+            str1 = Console.ReadLine();
+            Console.Write("文字列2を入力");
+            str2 = Console.ReadLine();
+            Console.WriteLine("文字列1+文字列2＝{0}", str1 + str2);
+        }
+    }
+}
