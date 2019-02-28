dcda541fec44dbeda4044cd38ec85d9a65512ec3
fukui-m <fukui-m@evoltech.co.jp>
2019-02-28 11:25:34 +0900

    一日目

---
+++ 1day study2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class test
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(123);
+            Console.WriteLine(456);
+            Console.Write("ABC");
+            Console.WriteLine("DEF");               
+        }
+    }
+}
