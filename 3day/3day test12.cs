d48b7ea7a56cf3491571dd95c904875332efd605
fukui-m <48060397+fukui-m@users.noreply.github.com>
2019-03-01 12:33:47 +0900

    Add files via upload

---
+++ 3day/3day test12.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力してください:");
+            int firstInputValse = int.Parse(Console.ReadLine());
+            if (firstInputValse < 0)
+            {
+                Console.WriteLine("負の値です");
+            }else if (0 < firstInputValse)
+            {
+                Console.WriteLine("正の値です");
+            }else
+            {
+                Console.WriteLine("0です");
+            }
+        }
+    }
+}
---
+++ 3day/3day test18.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3day
+{
+    class _3day_test18
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("温度を入力してください(-10～35):");
+            int firstInputValue = int.Parse(Console.ReadLine());
+            if (firstInputValue < -10 || 35 < firstInputValue)
+            {
+                Console.WriteLine("適切な値を入力してください。");
+            }
+            else if (0 <= firstInputValue && firstInputValue < 25)
+            {
+                Console.WriteLine("摂氏{0}度", firstInputValue);
+            }
+            else if (firstInputValue < 0)
+            {
+                Console.WriteLine("摂氏{0}度", firstInputValue);
+                Console.WriteLine("真冬日です");
+            }
+            else if (25 <= firstInputValue && firstInputValue < 30)
+            {
+                Console.WriteLine("摂氏{0}度", firstInputValue);
+                Console.WriteLine("");
+                Console.WriteLine("夏日です");
+            }else if (30 <= firstInputValue)
+            {
+                Console.WriteLine("摂氏{0}度", firstInputValue);
+                Console.WriteLine("");
+                Console.WriteLine("真夏日です");
+            }
+        }
+    }
+}
