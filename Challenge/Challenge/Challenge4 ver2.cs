c978c005ba2d88669806f992328324b5aeabeea8
fukui-m <48060397+fukui-m@users.noreply.github.com>
2019-03-06 14:52:46 +0900

    Add files via upload

---
+++ Challenge/Challenge/Challenge4 ver2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    class Challenge4_ver2
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            decimal dice = rnd.Next(1, 1001);
+            Console.Write("{0}は{1}桁です。", dice,dice.ToString().Length);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Challenge/Challenge/Challenge5 ver2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    class Challenge5_ver2
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("3で割り切れるか、数値の中に3が含まれる数");
+            for (int num = 1; num <= 100; num++)
+            {
+                if (num.ToString().Contains("3")||num % 3 == 0 )
+                {
+                    Console.WriteLine(num);
+                }
+                
+            }
+        }
+    }
+}
