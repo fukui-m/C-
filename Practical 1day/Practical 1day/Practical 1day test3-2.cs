2d0386e5f10b7a30b0e5e1129e36f63ff8cc14f0
fukui-m <48060397+fukui-m@users.noreply.github.com>
2019-03-07 10:43:08 +0900

    Add files via upload

---
+++ Practical 1day/Practical 1day/Practical 1day test3-2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_1day
+{
+    class Practical_1day_test3_2
+    {
+        public Practical_1day_test3_2()
+        {
+            Console.WriteLine("スタート");
+        }
+        public void Func()
+        {
+            Console.WriteLine("func");
+        }
+        ~Practical_1day_test3_2()
+        {
+            Console.WriteLine("エンド");
+        }
+    }
+}
---
+++ Practical 1day/Practical 1day/Practical 1day test3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_1day
+{
+    class Practical_1day_test3
+    {
+        static void Main(string[] args)
+        {
+            Practical_1day_test3_2 p = new Practical_1day_test3_2();
+            p.Func();
+        } 
+    }
+}
