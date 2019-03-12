f443d2330df3d880e9d6176848c8955bbf65bd4f
fukui-m <48060397+fukui-m@users.noreply.github.com>
2019-03-12 19:34:24 +0900

    Add files via upload

---
+++ Practical 7day/Practical 7day/Practical 7day study2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_7day
+{
+    class Practical_7day_study2
+    {
+        delegate void Action(int a);
+        static void Func1(int a)
+        {
+            Console.WriteLine("a={0}", a);
+        }
+        static void Func2(int a)
+        {
+            Console.WriteLine("a*2={0}", a * 2);
+        }
+        static void Func3(int a)
+        {
+            Console.WriteLine("a*3={0}", a * 3);
+        }
+        static void Main(string[] args)
+        {
+            Action a = new Action(Func1);
+            a += new Action(Func2);
+            a += new Action(Func3);
+            a(3);
+        }
+    }
+}
